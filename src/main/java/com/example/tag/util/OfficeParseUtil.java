1b98928caee7afc78c5df62c0a47696387c18d33
alex <296888680@qq.com>
2019-04-30 00:27:15 +0800

    use another jodcconverter module:com.artofsolving

--- src/main/java/com/example/tag/util/OfficeParseUtil.java
+++ src/main/java/com/example/tag/util/OfficeParseUtil.java
@@ -1,9 +1,8 @@
 package com.example.tag.util;
 
-import org.jodconverter.JodConverter;
-import org.jodconverter.office.LocalOfficeManager;
-import org.jodconverter.office.OfficeException;
-import org.jodconverter.office.OfficeUtils;
+import com.artofsolving.jodconverter.openoffice.connection.OpenOfficeConnection;
+import com.artofsolving.jodconverter.openoffice.connection.SocketOpenOfficeConnection;
+import com.artofsolving.jodconverter.openoffice.converter.OpenOfficeDocumentConverter;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,41 +10,17 @@ import java.io.IOException;
 public class OfficeParseUtil {
 
 
-    private static OfficeParseUtil officeParseUtil;
-
-    /**
-     * @return OfficeParseUtil
-     */
-    public static synchronized OfficeParseUtil getOfficeParseUtil() {
-        if (officeParseUtil == null) {
-            officeParseUtil = new OfficeParseUtil();
-        }
-        return officeParseUtil;
-    }
-
-    public static void main(String[] args) throws IOException, OfficeException {
+    public static void main(String[] args) throws IOException {
         File inputFile = new File("test.rtf");
-        File outputFile = new File("test1.docx");
-        // Create an office manager using the default configuration.
-        // The default port is 2002. Note that when an office manager
-        // is installed, it will be the one used by default when
-        // a converter is created.
-//        ((LocalOfficeManager.Builder)builder().install()).build();
-        final LocalOfficeManager officeManager = (LocalOfficeManager.builder().officeHome("/opt/openoffice4").killExistingProcess(true)
-                .install()).build();
-        try {
+        File outputFile = new File("test1.pdf");
+        OpenOfficeConnection connection = new SocketOpenOfficeConnection("127.0.0.1", 8100);
+        connection.connect();
 
-            // Start an office process and connect to the started instance (on port 2002).
-            officeManager.start();
-            // Convert
-            JodConverter
-                    .convert(inputFile)
-                    .to(outputFile)
-                    .execute();
-        } finally {
-            // Stop the office process
-            OfficeUtils.stopQuietly(officeManager);
-        }
+        // convert
+        OpenOfficeDocumentConverter converter = new OpenOfficeDocumentConverter(connection);
+        converter.convert(inputFile, outputFile);
+
+        connection.disconnect();
     }
 
 }
